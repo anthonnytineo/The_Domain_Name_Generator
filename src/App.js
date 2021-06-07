af1e5a480c694bc7f6f2d21b17781003990a4344
Anthonny Tineo Ruiz <anthonnytineo@MacBook-Pro-de-Anthonny.local>
2021-06-07 19:42:06 -0400

    reorganizacion del proyecto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Creaion</h1>
+      <h1>Creacion</h1>
     </div>
   );
 }
